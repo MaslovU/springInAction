379271e8cade47deab42bdb704c6976c60688dec
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2023-07-23 20:35:56 +0500

    added validation for fields

--- src/main/java/com/maslov/springinaction/controllers/DesignTacoController.java
+++ src/main/java/com/maslov/springinaction/controllers/DesignTacoController.java
@@ -7,8 +7,10 @@ import com.maslov.springinaction.models.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,9 +28,14 @@ public class DesignTacoController {
 
     @PostMapping
     public String processTaco(
-            Taco taco,
+            @Valid Taco taco,
+            Errors errors,
             @ModelAttribute TacoOrder tacoOrder
     ) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
--- src/main/java/com/maslov/springinaction/controllers/OrderController.java
+++ src/main/java/com/maslov/springinaction/controllers/OrderController.java
@@ -3,11 +3,14 @@ package com.maslov.springinaction.controllers;
 import com.maslov.springinaction.models.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -22,8 +25,12 @@ public class OrderController {
 
     @PostMapping
     public String processOrder(
-            TacoOrder tacoOrder,
+            @Valid TacoOrder tacoOrder,
+            Errors errors,
             SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", tacoOrder);
         sessionStatus.setComplete();
         return "redirect:/";
