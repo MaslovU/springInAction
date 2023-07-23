24bd6d5d6c72c1acee3c09b6a3798d00aeb8135d
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2023-07-23 12:36:53 +0500

    added design view

--- src/test/java/com/maslov/springinaction/controllers/HomeControllerTest.java
+++ src/test/java/com/maslov/springinaction/controllers/HomeControllerTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest(DesignTacoController.class)
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
