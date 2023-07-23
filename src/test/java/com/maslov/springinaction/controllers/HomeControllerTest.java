a8a7ea5617839df2e8c7bc7a303b66b2c73c391a
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2023-07-23 20:39:19 +0500

    fixed tests

--- src/test/java/com/maslov/springinaction/controllers/HomeControllerTest.java
+++ src/test/java/com/maslov/springinaction/controllers/HomeControllerTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(DesignTacoController.class)
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
