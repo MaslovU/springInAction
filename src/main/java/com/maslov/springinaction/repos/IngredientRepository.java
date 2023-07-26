3f0b2272fa520d71d9462036cd96d7d0cc41862c
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2023-07-26 11:21:57 +0500

    fixed tests

--- src/main/java/com/maslov/springinaction/repos/IngredientRepository.java
+++ src/main/java/com/maslov/springinaction/repos/IngredientRepository.java
@@ -3,8 +3,6 @@ package com.maslov.springinaction.repos;
 import com.maslov.springinaction.models.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
 }
--- src/test/java/com/maslov/springinaction/controllers/HomeControllerTest.java
+++ src/test/java/com/maslov/springinaction/controllers/HomeControllerTest.java
@@ -1,8 +1,10 @@
 package com.maslov.springinaction.controllers;
 
+import com.maslov.springinaction.repos.IngredientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -13,6 +15,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private IngredientRepository repository;
 
     @Test
     void testHomePage() throws Exception {
